fbd990b42aee8b0844ff6511a73d0dd9ebce123e
Fezao187 <katlegomaredi9@gmail.com>
2024-08-05 20:36:18 +0200

    Intro to pgAdmin

---
+++ book/_intro/intro.sql
@@ -0,0 +1,13 @@
+-- 1. How to download pgAdmin
+-- 2. Setting up pgAdmin
+-- 3. Query tool, run queries
+    -- a. Create table
+    CREATE TABLE pgTest{
+        testVal varchar(10)
+    };
+    -- b. Insert data
+    INSERT INTO pgTest (testVal)
+    VALUES ("Hello")
+    -- c. View data
+    SELECT * FROM pgTest
+-- 4. Save queries
