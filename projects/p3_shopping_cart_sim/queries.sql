fea089ea7e09cfe3f59bd6134536e0f3ef90914c
Fezao187 <katlegomaredi9@gmail.com>
2024-09-01 00:58:03 +0200

    Shopping Cart ERD

---
+++ projects/p3_shopping_cart_sim/queries.sql
@@ -0,0 +1,8 @@
+-- Create shopping cart database
+CREATE DATABASE "shopping_cart_db"
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LOCALE_PROVIDER = 'libc'
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
